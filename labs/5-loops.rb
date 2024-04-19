1398615cb5fcaaa4ab90fad8077de03cbb7fda82
Benjamin Block <bjblock@gmail.com>
2024-04-19 07:36:52 -0500

    simplifies lab
    uses multiple loops instead of nested loops

--- labs/5-loops.rb
+++ labs/5-loops.rb
@@ -3,11 +3,12 @@
 # ruby 5-loops.rb
 
 # EXERCISE
-# Build a deck of cards. Given the following arrays, use a loop
-# (or two... hint, hint) to write out every combination to the
-# screen.
-ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
-suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+# Build a deck of cards. Given the following arrays of card data,
+# use a loop to write out the cards to the screen.
+clubs = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
+diamonds = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
+hearts = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
+spades = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
 
 # Sample output:
 # 2 of Clubs
@@ -17,6 +18,13 @@ suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 # 3 of Clubs
 # ...
 
-# CHALLENGE
+# CHALLENGE #1
+# The arrays are identical for each suit and can be simplified by using
+# a ranks array as seen below. Try to complete the exercise again by
+# combining these arrays.
+ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
+suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+
+# CHALLENGE #2
 # Deal a poker hand. Shuffle the deck and "deal" (i.e. display) a 5 card hand (i.e. 5 cards from the deck).
 # You will want to look at the documentation for Arrays: https://ruby-doc.org/core-2.7.0/Array.html
